2e5e9290b722ac5a0f4dccb55452ab63112de207
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2021-06-14 16:46:38 +0100

    Implement action with actionTypes

---
+++ src/actions/index.js
@@ -0,0 +1,33 @@
+export const login = () => ({
+  type: types.LOGIN,
+});
+
+export const updateData = (name, data) => ({
+  type: types.UPDATE_DATA,
+  name,
+  data,
+});
+
+export const passStudy = data => ({
+  type: types.PASS_STUDIES,
+  data,
+});
+
+export const logout = () => ({
+  type: types.LOGOUT,
+});
+
+export const resetData = () => ({
+  type: types.LOGOUT,
+});
+
+export const createStudy = (name, data) => ({
+  type: types.CREATE_STUDY,
+  name,
+  data,
+});
+
+export const passProgress = data => ({
+  type: types.PASS_PROGRESS,
+  data,
+});
---
+++ src/constants/actionTypes.js
@@ -0,0 +1,6 @@
+export const UPDATE_DATA = 'UPDATE_DATA';
+export const LOGIN = 'LOGIN';
+export const LOGOUT = 'LOGOUT';
+export const PASS_STUDIES = 'PASS_STUDIES';
+export const CREATE_STUDY = 'CREATE_STUDY';
+export const PASS_PROGRESS = 'PASS_PROGRESS';
