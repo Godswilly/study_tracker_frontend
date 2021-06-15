2540caae3c660009056af176f7c91f97c69f9f32
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2021-06-15 15:24:18 +0100

    Implement handle login status

--- src/actions/index.js
+++ src/actions/index.js
@@ -42,3 +42,16 @@ export const handleLogout = () => (dispatch) => {
     dispatch(resetData());
   })
 }
+
+export const handleLoginStatus = (status) => (dispatch) => {
+  axios.get('http://localhost/api/v1/logged_in', { withCredentials:true })
+  .then(response => {
+    if(response.data.logged_in && status === 'NOT_LOGGED_IN') {
+      dispatch(login());
+      dispatch(updateData('email', response.data.user.email));
+    } else if (!response.data.logged_in && status === 'LOGGED_IN') {
+        dispatch(logout());
+        dispatch(resetData()); 
+      }
+  })
+};
