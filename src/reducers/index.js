ebd47ccd9a04d19a473ee7be3b5648a0eba7e88d
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2021-06-15 12:42:34 +0100

    Combine all the reducers in the index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,14 @@
 import { combineReducers } from 'redux';
+import progress from './progress';
+import status from './status';
+import studies from './studies';
+import study from './study';
+import user from './user';
 
 export default combineReducers ({
-  
-})
+  progress,
+  status,
+  studies,
+  study,
+  user,
+});
