696ec974852f49c99b0b0402eb807c37c803d4bb
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2021-06-18 17:15:51 +0100

    Update create store and bootstrap link

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/index.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/index';
 import App from './App';
 
+const store = createStore(rootReducer, {}, compose(applyMiddleware(thunk)));
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root'),
 );
